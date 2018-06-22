ce9a78cf5a6fb43093275e396a2f31537613f0be
root <root@clo.cryptomena.io>
2018-06-22 08:13:07 +0300

    update to unlocker

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -36,7 +36,7 @@ var uncleReward = new(big.Int).Div(constReward, new(big.Int).SetInt64(32))
 
 // Donate 5% from pool fees to developers
 const donationFee = 5.0
-const donationAccount = "0x34AE12692BD4567A27e3E86411b58Ea6954BA773"
+const donationAccount = "0x4bc7b9d69d6454c5666ecad87e5699c1ec02d533"
 
 type BlockUnlocker struct {
 	config   *UnlockerConfig
