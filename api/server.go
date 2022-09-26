614a37236cea77f0151c7545e1729e9cb31c2066
tanakaradiv <106475433+tanakaradiv@users.noreply.github.com>
2022-09-26 21:55:13 +0700

    Update server.go

--- api/server.go
+++ api/server.go
@@ -14,8 +14,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/robfig/cron"
 
-	"github.com/chainkorea/open-callisto-pool/storage"
-	"github.com/chainkorea/open-callisto-pool/util"
+	"github.com/tanakaradiv/open-callisto-pool/storage"
+	"github.com/tanakaradiv/open-callisto-pool/util"
 )
 
 type ApiConfig struct {
