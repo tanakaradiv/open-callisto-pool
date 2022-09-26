7e73633596ec82f8c3e097fe68acb06f58f84fbe
tanakaradiv <106475433+tanakaradiv@users.noreply.github.com>
2022-09-26 22:09:45 +0700

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"errors"
 
-	"github.com/chainkorea/open-callisto-pool/rpc"
-	"github.com/chainkorea/open-callisto-pool/util"
+	"github.com/tanakaradiv/open-callisto-pool/rpc"
+	"github.com/tanakaradiv/open-callisto-pool/util"
 )
 
 // Allow only lowercase hexadecimal with 0x prefix
