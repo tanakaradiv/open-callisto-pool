1e24b3b8ff76de6a30217cd2ba47f348361407f4
tanakaradiv <106475433+tanakaradiv@users.noreply.github.com>
2022-09-26 22:02:57 +0700

    Update policy.go

--- policy/policy.go
+++ policy/policy.go
@@ -9,8 +9,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/chainkorea/open-callisto-pool/storage"
-	"github.com/chainkorea/open-callisto-pool/util"
+	"github.com/tanakaradiv/open-callisto-pool/storage"
+	"github.com/tanakaradiv/open-callisto-pool/util"
 )
 
 type Config struct {
